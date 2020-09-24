37101d93fc3cb103546b2590866a45c618162192
PogoL <patryk.pogorzelczyk@gmail.com>
2020-09-24 10:28:34 +0200

    5.87 my solution for passing clicked recipe to another component

--- recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -8,15 +8,20 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('A test recipe', 'simply test recipe', 
+    new Recipe('Cocktail', 'better test recipe', 
     'https://previews.123rf.com/images/scyther5/scyther51703/scyther5170300058/73650702-cooking-food-kitchen-cutting-cook-hands-man-male-knife-preparation-fresh-preparing-hand-table-salad-.jpg'),
     new Recipe('A test recipe', 'simply test recipe', 
     'https://previews.123rf.com/images/scyther5/scyther51703/scyther5170300058/73650702-cooking-food-kitchen-cutting-cook-hands-man-male-knife-preparation-fresh-preparing-hand-table-salad-.jpg')
   ];
 
+  @Output() displayRecipe = new EventEmitter<Recipe>();
+
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  
+  onRecipeClicked(clickedRecipe: Recipe) {
+    this.displayRecipe.emit(clickedRecipe);
+  }
 }
