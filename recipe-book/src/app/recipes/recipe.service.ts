18ebe3e0eb931a12a7bfd8218700c45cd187ae0a
PogoL <patryk.pogorzelczyk@gmail.com>
2020-09-28 11:21:08 +0200

    add ingredients array to recipe model

--- recipe-book/src/app/recipes/recipe.service.ts
+++ recipe-book/src/app/recipes/recipe.service.ts
@@ -1,4 +1,5 @@
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 import { Recipe } from './recipe.model';
 
 export class RecipeService {
@@ -6,10 +7,22 @@ export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
 
     recipes: Recipe[] = [
-        new Recipe('Cocktail', 'better test recipe',
-            'https://previews.123rf.com/images/scyther5/scyther51703/scyther5170300058/73650702-cooking-food-kitchen-cutting-cook-hands-man-male-knife-preparation-fresh-preparing-hand-table-salad-.jpg'),
-        new Recipe('A test recipe', 'simply test recipe',
-            'https://previews.123rf.com/images/scyther5/scyther51703/scyther5170300058/73650702-cooking-food-kitchen-cutting-cook-hands-man-male-knife-preparation-fresh-preparing-hand-table-salad-.jpg')
+        new Recipe(
+            'Cocktail', 
+            'better test recipe',
+            'https://previews.123rf.com/images/scyther5/scyther51703/scyther5170300058/73650702-cooking-food-kitchen-cutting-cook-hands-man-male-knife-preparation-fresh-preparing-hand-table-salad-.jpg',
+            [
+                new Ingredient('Milk', 2),
+                new Ingredient('Strawberries', 10)
+            ]),
+        new Recipe(
+            'Fish and fries',
+            'It\'s just a fish and some fries',
+            'https://upload.wikimedia.org/wikipedia/commons/thumb/5/55/Fish%2C_chips_and_mushy_peas.jpg/800px-Fish%2C_chips_and_mushy_peas.jpg',
+            [
+                new Ingredient('Fish', 1),
+                new Ingredient('Fries', 20)
+            ])
     ];
 
     getRecipes(): Recipe[] {
